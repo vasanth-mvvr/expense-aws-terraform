7b9c9d1cf14f39f573aa0a3e771ac9ee679c0b72
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-09-02 10:53:01 +0530

    latest

---
+++ output.tf
@@ -0,0 +1,31 @@
+# output "az" {
+#   value = data.aws_availability_zones.az.names
+# }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_ids" {
+  value = aws_db_subnet_group.default.id
+}
+
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}

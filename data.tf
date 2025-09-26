0b2ec9edf51f9d1933fb2e75a558da24bb5e3489
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-09-26 09:58:03 +0530

    latest

--- data.tf
+++ data.tf
@@ -6,13 +6,6 @@ data "aws_availability_zones" "az" {
   state = "available"
 }
 
-# data "acceptor_vpc_id" "acceptor" {
-  
-# }
-
-# data "aws_vpc" "default" {
-#   default = true
-# }
 data "aws_route_table" "main" {
   vpc_id = data.aws_vpc.default.id
   filter {
